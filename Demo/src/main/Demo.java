9a5976a4c3666717613e766f4e08899379e000a8
zhangchunyugithub <zcy@99666@126.com>
2019-05-27 16:45:30 +0800

    一个输出；

---
+++ Demo/src/main/Demo.java
@@ -0,0 +1,15 @@
+package main;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import model.Result;
+
+import org.junit.Test;
+
+public class Demo {
+	public static void main(String[] args){
+		System.out.println(111);
+	}
+}
